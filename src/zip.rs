a7243c8831fff8fc879da86ae77205e0f8303ead
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2022-08-21 01:30:13 +0200

    just store it as bytes then

--- src/zip.rs
+++ src/zip.rs
@@ -14,7 +14,7 @@ pub fn read_rom(path: impl AsRef<Path>) -> Result<Vec<NamedFile>, Box<dyn Error>
     let mut archive = ZipArchive::new(reader)?;
 
     (0..archive.len()).map(|index| {
-        let mut file = archive.by_index_decrypt(index, PASS.as_bytes())??;
+        let mut file = archive.by_index_decrypt(index, PASS)??;
         let mut bytes = Vec::with_capacity(file.size() as usize);
         file.read_to_end(&mut bytes)?;
         Ok((file.name().to_owned(), bytes))
